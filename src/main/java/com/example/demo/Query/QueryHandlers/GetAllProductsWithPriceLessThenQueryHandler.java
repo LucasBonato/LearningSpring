2f6f89c5ca21f65011957a745985a4383ec53d89
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-03-17 22:10:00 -0300

    [REFACTOR] Utilização de Data JPA para query

--- src/main/java/com/example/demo/Query/QueryHandlers/GetAllProductsWithPriceLessThenQueryHandler.java
+++ src/main/java/com/example/demo/Query/QueryHandlers/GetAllProductsWithPriceLessThenQueryHandler.java
@@ -1,6 +1,7 @@
 package com.example.demo.Query.QueryHandlers;
 
 import com.example.demo.Models.DTOs.ProductDTO;
+import com.example.demo.Models.Product;
 import com.example.demo.Query.Query;
 import com.example.demo.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,11 @@ public class GetAllProductsWithPriceLessThenQueryHandler implements Query<Double
 
     @Override
     public ResponseEntity<List<ProductDTO>> execute(Double maxPrice) {
-        List<ProductDTO> response = productRepository.findProductsWithPriceLessThan(maxPrice);
+        List<ProductDTO> response = productRepository
+                .findByPriceLessThan(maxPrice)
+                .stream()
+                .map(ProductDTO::new)
+                .toList();
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/demo/Repositories/ProductRepository.java
+++ src/main/java/com/example/demo/Repositories/ProductRepository.java
@@ -4,16 +4,13 @@ import com.example.demo.Models.DTOs.ProductDTO;
 import com.example.demo.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    @Query("SELECT new com.example.demo.Models.DTOs.ProductDTO(name, description, price) FROM Product p WHERE p.price < :maxPrice")
-    List<ProductDTO> findProductsWithPriceLessThan(@Param(value = "maxPrice") Double maxValue);
-
+    List<Product> findByPriceLessThan(Double maxValue);
     @Query("SELECT new com.example.demo.Models.DTOs.ProductDTO(name, description, price) FROM Product")
     List<ProductDTO> getAllProductsDTOs();
 }
