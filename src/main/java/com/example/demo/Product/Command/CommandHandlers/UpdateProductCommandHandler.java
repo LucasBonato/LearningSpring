9da3ee22e3026973334dd1d9c91983f2f8fda460
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-03-21 23:24:24 -0300

    [OPTMIZATION] Use cache to respond the users faster, getting it and puting in the cache, updating the cache when the user updates as well.

--- src/main/java/com/example/demo/Product/Command/CommandHandlers/UpdateProductCommandHandler.java
+++ src/main/java/com/example/demo/Product/Command/CommandHandlers/UpdateProductCommandHandler.java
@@ -1,19 +1,23 @@
 package com.example.demo.Product.Command.CommandHandlers;
 
 import com.example.demo.Product.Command.Command;
+import com.example.demo.Product.Models.DTOs.ProductDTO;
 import com.example.demo.Product.Models.Product;
 import com.example.demo.Repository.ProductRepository;
 import com.example.demo.Product.Util.ValidateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UpdateProductCommandHandler implements Command<UpdateProductCommand, Void> {
+public class UpdateProductCommandHandler implements Command<UpdateProductCommand, ResponseEntity> {
     @Autowired
     private ProductRepository productRepository;
     @Override
-    public ResponseEntity<Void> execute(UpdateProductCommand command) {
+    @CachePut(value = "productCache", key = "#command.getId()")
+    public ResponseEntity execute(UpdateProductCommand command) {
         Product product = command.getProduct();
         Integer id = command.getId();
 
@@ -24,6 +28,8 @@ public class UpdateProductCommandHandler implements Command<UpdateProductCommand
         ValidateUtils.validateProduct(product);
 
         productRepository.save(product);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new ProductDTO(product));
     }
 }
+// Tomar cuidado, pois com o cache, QUALQUER coisa que ele retornar, será colocada no Cache mesmo que não faça sentido
+// ou seja, tem que retornar o objeto que foi atualizado.
--- src/main/java/com/example/demo/Product/Query/QueryHandlers/GetProductQueryHandler.java
+++ src/main/java/com/example/demo/Product/Query/QueryHandlers/GetProductQueryHandler.java
@@ -6,6 +6,7 @@ import com.example.demo.Product.Models.Product;
 import com.example.demo.Product.Query.Query;
 import com.example.demo.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class GetProductQueryHandler implements Query<Integer, ProductDTO> {
     @Autowired
     private ProductRepository productRepository;
     @Override
+    @Cacheable("productCache")
     public ResponseEntity<ProductDTO> execute(Integer id) {
         Optional<Product> optionalProduct = productRepository.findById(id);
         if(optionalProduct.isPresent()){
