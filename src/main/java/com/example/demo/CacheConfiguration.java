08921d74db0df20cac0c6193ddb2a01eb8e69a17
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-03-21 23:41:09 -0300

    [BUILD] Scheduling cache evict(despejo) in the cache configuration.

--- src/main/java/com/example/demo/CacheConfiguration.java
+++ src/main/java/com/example/demo/CacheConfiguration.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Arrays;
 
@@ -13,6 +15,8 @@ import java.util.Arrays;
 @EnableCaching
 @EnableScheduling
 public class CacheConfiguration {
+    private static final int CINCO_MINUTOS = 5 * 60000;
+
     @Bean
     public CacheManager cacheManager(){
         ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
@@ -20,4 +24,8 @@ public class CacheConfiguration {
         cacheManager.setCacheNames(Arrays.asList("productCache", "clienteCache")); // Os conjuntos de cache da RAM
         return cacheManager;
     }
+
+    @CacheEvict(value = "productCache", allEntries = true)
+    @Scheduled(fixedDelay = CINCO_MINUTOS, initialDelay = 0) // A cada 5 segundos ele vai despejar os dados do banco no cache
+    public void evictProductCache(){ }
 }
